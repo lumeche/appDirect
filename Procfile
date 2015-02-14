3b34130ed21bf8f4611ad8a3d2392b2222dc3378
lumeche <lumeche@gmail.com>
2015-02-14 16:26:23 -0500

    Variable ports

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
- web:    java -jar target/challenge-1.0-SNAPSHOT.jar
+ web:    java $JAVA_OPTS -jar target/challenge-1.0-SNAPSHOT.jar
--- src/main/java/com/appdirect/Application.java
+++ src/main/java/com/appdirect/Application.java
@@ -13,6 +13,11 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 
     public static void main(String[] args) {
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+        System.setProperty("server.port", webPort);
         SpringApplication.run(Application.class, args);
     }
 }
