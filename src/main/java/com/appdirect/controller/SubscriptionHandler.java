efad15525d231fc4e4f053e4b4a5a4108768d652
lumeche <lumeche@gmail.com>
2015-02-14 20:41:22 -0500

    add more logs

--- src/main/java/com/appdirect/controller/SubscriptionHandler.java
+++ src/main/java/com/appdirect/controller/SubscriptionHandler.java
@@ -1,6 +1,9 @@
 package com.appdirect.controller;
 
 import com.appdirect.controller.response.AccountResponse;
+import com.appdirect.model.utils.LoggerUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,24 +17,30 @@ import java.util.UUID;
 
 @RestController
 public class SubscriptionHandler {
+    final static Logger logger = LoggerFactory.getLogger(IsAlive.class);
+
     @RequestMapping("/subscription/create")
     public AccountResponse subscriptionCreate(@RequestParam(value = "token",required = true)String token,Model model){
+        LoggerUtils.logDebug(logger,"Subscription create received");
         return buildAccountResponse();
     }
 
 
     @RequestMapping("/subscription/change")
     public AccountResponse subscriptionChange(@RequestParam(value = "token",required = true)String token,Model model){
+        LoggerUtils.logDebug(logger,"Subscription change received");
         return buildAccountResponse();
     }
 
     @RequestMapping("/subscription/cancel")
     public AccountResponse subscriptionCancel(@RequestParam(value = "token",required = true)String token,Model model){
+        LoggerUtils.logDebug(logger,"Subscription delete received");
         return buildAccountResponse();
     }
 
     @RequestMapping("/subscription/status")
     public AccountResponse subscriptionStatus(@RequestParam(value = "token",required = true)String token,Model model){
+        LoggerUtils.logDebug(logger,"Subscription status received");
         return buildAccountResponse();
     }
 
