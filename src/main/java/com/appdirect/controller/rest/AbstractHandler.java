b63606ab89e5c702500db12488dbd5ffc0ba22e6
lumeche <lumeche@gmail.com>
2015-02-16 19:35:53 -0500

    Fixing the problem with the JSON

--- src/main/java/com/appdirect/controller/rest/AbstractHandler.java
+++ src/main/java/com/appdirect/controller/rest/AbstractHandler.java
@@ -8,10 +8,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.xml.transform.Source;
+import java.util.Arrays;
 
 /**
  * Created by Luis Tobon on 2015-02-15.
@@ -29,9 +31,17 @@ public abstract class AbstractHandler {
 
     protected String getEventInfo(String token) {
         LoggerUtils.logDebug(logger, "About to send GET request to %s with token %s", appDirectEventsURL, token);
-        String event=oauthRestTemplate.getForObject(appDirectEventsURL,String.class,token);
+        ResponseEntity<String> response = sendGetRequest(token);
+        String event=response.getBody();
         LoggerUtils.logDebug(logger,"Event  returned %s",event.toString());
         return event;
+    }
+
+    private ResponseEntity<String> sendGetRequest(String token) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_XML));
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+        return oauthRestTemplate.exchange(appDirectEventsURL, HttpMethod.GET,entity,String.class,token);
     }
 
 
