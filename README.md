208666aa865f0182145e4de48e7c1dc11bb51d66
lumeche <lumeche@gmail.com>
2015-02-21 22:28:30 -0500

    More messagwes

--- README.md
+++ README.md
@@ -8,4 +8,14 @@ BUILD APPLICATION
 In order to compile run "mvn clean package". It will generate a target folder where the jar file is located.
 
 START SERVER
-To start the server run "java -jar
+To start the server run "java -jar ./target/appDirect-1.0-SNAPSHOT.jar.
+This will start the server by default in 8080
+
+ACCEDING THE SERVER
+The server has published two handlers.
+
+/isAlive
+Simple page that shows if the server is alive
+
+/
+Contains the information about the excising subscriptions and the users Assigned.
--- src/main/java/com/appdirect/controller/rest/SubscriptionHandler.java
+++ src/main/java/com/appdirect/controller/rest/SubscriptionHandler.java
@@ -10,12 +10,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by Luis Tobon on 2015-02-14.
@@ -115,4 +115,5 @@ public class SubscriptionHandler {
     public void setHandlerDelegate(HandlerDelegate handlerDelegate) {
         this.handlerDelegate = handlerDelegate;
     }
+
 }
